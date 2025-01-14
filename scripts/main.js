253a75c3b4dbc94649336b24114e9cc284ef1784
Maksym Riazanov <maksym.riazanov@md.p2h.com>
2025-01-14 13:14:02 +0200

    add accordion

--- scripts/main.js
+++ scripts/main.js
@@ -157,6 +157,17 @@ document.addEventListener("DOMContentLoaded", () => {
     linkInput.value = "";
   });
 
+  document.querySelectorAll('.accordion-header').forEach(header => {
+    header.addEventListener('click', () => {
+        const content = header.nextElementSibling;
+        const isActive = content.style.display === 'block';
+        
+        document.querySelectorAll('.accordion-content').forEach(item => item.style.display = 'none');
+        
+        if (!isActive) content.style.display = 'block';
+    });
+});
+
   // Initializing tasks
   loadTasks();
 });
